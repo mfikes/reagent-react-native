fc27564787f13e0156431d5b858b5bad80929d8b
Mike Fikes <mike@fikesfarm.com>
2015-07-20 16:21:44 -0400

    Elininate interim screen text

--- AwesomeProject/index.ios.js
+++ AwesomeProject/index.ios.js
@@ -15,18 +15,7 @@ var {
 var AwesomeProject = React.createClass({
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <View/>
     );
   }
 });
